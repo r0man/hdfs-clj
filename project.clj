e714d9ccc427a71d59e93391d2aa6dbd23200257
Roman Scherer <roman.scherer@nugg.ad>
2012-06-06 10:34:39 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject hdfs "0.1.0-SNAPSHOT"
+  :description "A Clojure HDFS library."
+  :url "https://github.com/r0man/hdfs-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
