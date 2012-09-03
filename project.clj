4480e5384f1b071364a507823167f903265ae223
Roman Scherer <roman.scherer@nugg.ad>
2012-09-03 15:26:29 +0200

    Added deftool and run-tool.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hdfs-clj "0.1.2"
+(defproject hdfs-clj "0.1.3-SNAPSHOT"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
---
+++ src/hdfs/tool.clj
@@ -0,0 +1,38 @@
+(ns hdfs.tool
+  (:import org.apache.hadoop.conf.Configuration
+           org.apache.hadoop.util.ToolRunner))
+
+(defmacro deftool [[tool args] & body]
+  (let [clazz (symbol (namespace-munge (str *ns*)))]
+    `(do
+       (gen-class
+        :extends "org.apache.hadoop.conf.Configured"
+        :implements ["org.apache.hadoop.util.Tool"]
+        :name ~clazz
+        :state ~'state
+        :init ~'init
+        :constructors {[] []}
+        :main true)
+       (defn ~'-init []
+         [[] (atom nil)])
+       (defn ~'-run [~tool ~args]
+         (let [result# (do ~@body)]
+           (if (number? result#)
+             result# 0)))
+       (defn ~'-main [& ~'args]
+         (System/exit
+          (ToolRunner/run
+           (Configuration.)
+           (clojure.lang.Reflector/invokeConstructor
+            (resolve (symbol ~(str clazz))) (into-array []))
+           (into-array String ~'args))))
+       (defn ~'-getConf [~'tool]
+         (deref (.state ~'tool)))
+       (defn ~'-setConf [~'tool ~'conf]
+         (if ~'conf (swap! (.state ~'tool) (constantly ~'conf)))))))
+
+(defn run-tool [clazz & args]
+  (.run (clojure.lang.Reflector/invokeConstructor
+         (resolve (symbol (str clazz)))
+         (into-array []))
+        (into-array String args)))
