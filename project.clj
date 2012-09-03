656a7fb02fd127f419cd3833b9a2f0fef776f5e9
Roman Scherer <roman.scherer@nugg.ad>
2012-09-03 10:54:21 +0200

    Release 0.1.2.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject hdfs-clj "0.1.2-SNAPSHOT"
+(defproject hdfs-clj "0.1.2"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u3"]]}}
+  :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u5"]]}}
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}  )
