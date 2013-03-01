4d239283ea9338efaab3a13225dfe17b622ceef6
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:20:39 +0100

    Switched to Clojure 1.5.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject hdfs-clj "0.1.6-SNAPSHOT"
+(defproject hdfs-clj "0.1.7"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u5"]]}}
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}  )
