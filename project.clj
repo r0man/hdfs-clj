df5cad0ffdd2dfbb1a2469d261643c6c57544e39
Roman Scherer <roman.scherer@nugg.ad>
2013-08-21 10:55:53 +0200

    Release 0.1.8.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hdfs-clj "0.1.8-SNAPSHOT"
+(defproject hdfs-clj "0.1.8"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
