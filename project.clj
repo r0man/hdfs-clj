da274dcd53ae0d376541344dbde22a477921fd78
Roman Scherer <roman.scherer@nugg.ad>
2014-01-06 11:17:38 +0100

    lein-release plugin: preparing 0.1.12 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hdfs-clj "0.1.12-SNAPSHOT"
+(defproject hdfs-clj "0.1.12"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
