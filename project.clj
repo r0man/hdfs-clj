4d87631dc8110530f5ed177ceaa5c611c6f2c52f
Doug Selph <dselph@gmail.com>
2015-02-11 20:06:45 -0600

    Update clojure version, and update hadoop java lib version.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject hdfs-clj "0.1.14-SNAPSHOT"
+(defproject hdfs-clj "0.1.15"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u6"]]}}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-common "2.5.0-cdh5.2.3"]]}}
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}  )
