d9adacf1886227562d776d933f4a9795718a1e8b
Doug Selph <dselph@gmail.com>
2015-08-11 10:11:31 -0500

    Update Clojure version.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-common "2.5.0-cdh5.2.3"]]}}
   :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}  )
