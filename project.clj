af83d2ead8027679ef84a5ee1a67f7f2524a8545
Roman Scherer <roman@burningswell.com>
2016-03-31 17:58:56 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-common "2.5.0-cdh5.2.3"]]}}
-  :repositories {"cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}  )
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:provided {:dependencies [[org.apache.hadoop/hadoop-common "2.7.2"]]}})
