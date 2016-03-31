36e4c8ffc743e548051cadc67a54057eea12ecc9
Roman Scherer <roman@burningswell.com>
2016-03-31 18:28:38 +0200

    Version 0.1.16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hdfs-clj "0.1.16-SNAPSHOT"
+(defproject hdfs-clj "0.1.16"
   :description "A Clojure HDFS library."
   :url "https://github.com/r0man/hdfs-clj"
   :license {:name "Eclipse Public License"
